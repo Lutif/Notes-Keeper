24448e1fb9d9da4cd2d6c7646c5644d8c382de89
Lutif <lmandhro@gmail.com>
2020-01-15 19:19:07 +0500

    Updates

---
+++ precache-manifest.8815dec2c5aec22debf2cc620563c432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b1654a4d3ae3c38d446b8a6ecf9983",
+    "url": "/Notes-Keeper/index.html"
+  },
+  {
+    "revision": "506e3c2771124da09a89",
+    "url": "/Notes-Keeper/static/css/main.bf570700.chunk.css"
+  },
+  {
+    "revision": "5dc536f33670f94f2dba",
+    "url": "/Notes-Keeper/static/js/2.6ddb7bce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Notes-Keeper/static/js/2.6ddb7bce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "506e3c2771124da09a89",
+    "url": "/Notes-Keeper/static/js/main.6262aa6a.chunk.js"
+  },
+  {
+    "revision": "88181f8251903bcfbe0e",
+    "url": "/Notes-Keeper/static/js/runtime-main.4bc88774.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Notes-Keeper/static/media/logo.5d5d9eef.svg"
+  }
+]);
